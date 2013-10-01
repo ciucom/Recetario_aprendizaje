b7480dd7f1da3cf3de9acee3249305cd3d90f224
ciu <contacto@diegonaranjo.co>
2013-10-01 07:14:28 -0500

    segundo

--- recetario/principal/views.py
+++ recetario/principal/views.py
@@ -3,6 +3,25 @@ from principal.models import Bebida, Receta, Comentario
 from django.contrib.auth.models import User
 from django.http import HttpResponse
 from django.shortcuts import render_to_response, get_object_or_404
+from django.template import RequestContext
+
+def inicio(request):
+	recetas = Receta.objects.all()
+	return render_to_response('inicio.html',{'recetas':recetas})
+
+def usuarios(request):
+	usuarios = User.objects.all()
+	recetas = Receta.objects.all()
+	return render_to_response('usuarios.html',{'usuarios':usuarios,'recetas':recetas})
+
+def lista_recetas(request):
+	recetas = Receta.objects.all()
+	return render_to_response ('recetas.html',{'datos':recetas}, context_instance = RequestContext(request))
+
+def detalle_receta(request,id_receta):
+	dato = get_object_or_404(Receta, pk=id_receta)
+	comentarios = Comentario.objects.filter(receta=dato)
+	return render_to_response('receta.html',{'receta':dato,'comentarios':comentarios},context_instance=RequestContext(request))
 
 def index(request):
 	receta = Receta.objects.all()
@@ -15,3 +34,7 @@ def lista_bebidas(request):
 def sobre(request):
 	html = "<html><body>Proyecto de ejemplo en MDW</body></html>"
 	return HttpResponse(html)
+
+
+
+
--- recetario/recetario/urls.py
+++ recetario/recetario/urls.py
@@ -8,14 +8,16 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'recetario.views.home', name='home'),
     # url(r'^recetario/', include('recetario.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
+    # Uncomment the admin/doc line below to enable admin documentation:  
     # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
     #url(r'^$','principal.views.lista_bebidas'),
-    url(r'^$','principal.views.index'),
+    #url(r'^$','principal.views.index'),  
+    url(r'^$','principal.views.inicio'),
+    url(r'^usuarios/$','principal.views.usuarios'),
+    url(r'^recetas/$','principal.views.lista_recetas'),
+    url(r'^receta/(?P<id_receta>\d+)$','principal.views.detalle_receta'),    
     url(r'^sobre/$','principal.views.sobre'),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^media/(?P<path>.*)$','django.views.static.serve',{'document_root':settings.MEDIA_ROOT,}),
 )
